98fa194f352ebcfa005451e913166aad253daac3
Emil Palm <emil@md5.nu>
2011-12-19 02:25:21 +0100

    Missed some stuff

--- build/spotify.js
+++
@@ -1,5 +0,0 @@
-(function() {
-  window.sp = getSpotifyApi(1);
-  window.views = sp.require("sp://import/scripts/api/views");
-  $.extend(window, sp.require("sp://import/scripts/api/models"));
-}).call(this);
